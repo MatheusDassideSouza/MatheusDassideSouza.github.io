4636be0223a9a1a054188d077f3d402e3d2a28d3
Matheus D <130712347+MatheusDassideSouza@users.noreply.github.com>
2023-11-27 20:26:59 -0300

    Add files via upload

---
+++ aulas/Aula 1/aula1.js
@@ -0,0 +1,18 @@
+//Faz um aleta ao usuário
+a = 1
+b = a + 2
+
+window.alert('O resultado é: ' + b);
+
+nome = 'Matheus'
+
+alert('O meu nome é: ' + nome)
+
+//Escreve uma mensagem no console da DevTools
+console.log('Testando console');
+
+//Escreve uma mensagem no site, sem polimento, está no primeiro posição que ficar
+document.write('Testando write');
+
+//Adiciona algo a um elemento vazio, pode adicionar tags de HTML.
+document.getElementById('titulo').innerHTML = '<mark>Olá pessoal!</mark>';
